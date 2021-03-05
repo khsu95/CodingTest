a76395ed2d404a0357c2126f57a2253d75555e85
khsu95 <48276591+khsu95@users.noreply.github.com>
2021-03-06 02:17:40 +0900

    Still Now Working...In Case of Simultaneous Exit

--- 13460.cpp
+++ 13460.cpp
@@ -18,31 +18,36 @@ typedef struct elem
 	int dir = -1;
 } Elem;
 
-void Move(vector<vector<char>> table, ii &ball, int dir)
+bool Move(vector<vector<char>> table, ii& ball, int dir)
 {
 	int x = ball.first, y = ball.second;
-	while ((x>=0)&&(x<table.size())&&(y>=0)&&(y<table[0].size()))
+	while ((x >= 0) && (x < table.size()) && (y >= 0) && (y < table[0].size()))
 	{
 		//This is Possible, Because Table' Boundary is #
-		if ((table[x + dx[dir]][y + dy[dir]] != '.')&& (table[x + dx[dir]][y + dy[dir]] != 'O'))	break;
+		if ((table[x + dx[dir]][y + dy[dir]] != '.') && (table[x + dx[dir]][y + dy[dir]] != 'O'))	break;
 		x += dx[dir];
 		y += dy[dir];
 		if (table[x][y] == 'O')	break;
 	}
+	if (ii(x, y) == ball)	return 0;
 	ball = ii(x, y);
+	return 1;
 }
 
-void Swap(vector<vector<char>>& table, ii a, ii b, ii dest)
+bool Swap(vector<vector<char>>& table, ii a, ii b, ii dest)
 {
-	if (a == b)	return;
 	if (b == dest)
+	{
 		table[a.first][a.second] = '.';
+		return 1;
+	}
 	else
 	{
 		char temp = table[a.first][a.second];
 		table[a.first][a.second] = table[b.first][b.second];
 		table[b.first][b.second] = temp;
 	}
+	return 0;
 }
 
 int main(int argc, char** argv)
@@ -50,12 +55,12 @@ int main(int argc, char** argv)
 	freopen("input.txt", "r", stdin);
 	int col, row, ans = -1;
 	ii dest;
-	cin >> col>> row;
+	cin >> col >> row;
 	Elem First_Stage;
 	First_Stage.table.assign(col, vector<char>(row, 0));
-	for (int i = 0;i < col;i++) 
+	for (int i = 0;i < col;i++)
 	{
-		for (int j = 0;j < row;j++) 
+		for (int j = 0;j < row;j++)
 		{
 			cin >> First_Stage.table[i][j];
 			if (First_Stage.table[i][j] == 'R')	First_Stage.red = ii(i, j);
@@ -79,29 +84,20 @@ int main(int argc, char** argv)
 			if ((Now.dir == 2) && (i == 3))	continue;
 			if ((Now.dir == 3) && (i == 2))	continue;
 			Elem Next = Now;
-			if ((Now.red.first + dx[i] == Now.blue.first) && (Now.red.second + dy[i] == Now.blue.second))
-			{
-				//Blue First, Because Blue Blocks Red' Path
-				Move(Next.table, Next.blue, i);
-				Swap(Next.table, Now.blue, Next.blue, dest);
-				Move(Next.table, Next.red, i);
+			//Must Go to The END
+			if (Move(Next.table, Next.blue, i))
+				if (Swap(Next.table, Now.blue, Next.blue, dest))
+					continue;
+			if(Move(Next.table, Next.red, i))
 				Swap(Next.table, Now.red, Next.red, dest);
-			}
-			else
-			{
-				//Red First
-				Move(Next.table, Next.red, i);
-				Swap(Next.table, Now.red, Next.red,dest);
-				Move(Next.table, Next.blue, i);
-				Swap(Next.table, Now.blue, Next.blue,dest);
-			}
+			if (Move(Next.table, Next.blue, i))
+				if (Swap(Next.table, Now.blue, Next.blue, dest))
+					continue;
 			Next.dir = i;
 			Next.cnt = Now.cnt + 1;
 
-			if ((Now.blue == Next.blue) && (Now.red == Next.red))	continue;
 			//This is Possible, Because Coordination of Destination is Stored in dest
-			if (Next.blue == dest)	continue;
-			if (Next.red == dest) 
+			if (Next.red == dest)
 			{
 				ans = Next.cnt;
 				i = 5;
