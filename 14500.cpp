a7ae433321243e356c8a3a46fd243bb89f92700c
khsu95 <48276591+khsu95@users.noreply.github.com>
2021-03-07 06:41:01 +0900

    Give up DFS,

--- 14500.cpp
+++ 14500.cpp
@@ -3,43 +3,76 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
 #include <vector>
-#include <bitset>
-#include <queue>
+#include <algorithm>
 
 using namespace std;
 
 typedef pair<int, int> ii;
-typedef struct node
-{
-	int x = 0;
-	int y = 0;
-	int cnt = 0;
-	int sum = 0;
-}Node;
 
 int ans = 0;
 
 
-//Another Type
+//Type
+// 4Square
+void I_Type(vector<vector<int>> table)
+{
+	int sum = 0;
+	for (int col = 0;col < table.size();col++)
+	{
+		for (int row = 0;row < table[0].size() - 3;row++)
+		{
+			sum = table[col][row] + table[col][row + 1] + table[col][row + 2] + table[col][row + 3];
+			ans = (sum > ans) ? sum : ans;
+		}
+	}
+}
+
+//3 Square
 void T_Type(vector<vector<int>> table)
 {
 	int sum = 0;
 	for (int col = 0;col < table.size();col++)
 	{
-		for (int row = 1;row < table[0].size() - 1;row++)
+		for (int row = 0;row < table[0].size() - 2;row++)
 		{
-			sum = table[col][row - 1] + table[col][row] + table[col][row + 1];
+			sum = table[col][row] + table[col][row + 1] + table[col][row + 2];
 			int up = 0, down = 0;
-			if (col - 1 >= 0)
-				up = table[col - 1][row];
-			if (col + 1 < table.size())
-				down = table[col + 1][row];
-
+			for (int i = 0;i < 3;i++)
+			{
+				if(col-1>=0)
+					if (table[col - 1][row + i] > up)
+						up = table[col - 1][row + i];
+				if(col+1<table.size())
+					if (table[col + 1][row + i] > down)
+						down = table[col + 1][row + i];
+			}
 			sum += ((up > down) ? up : down);
 			ans = (sum > ans) ? sum : ans;
 		}
 	}
 }
+
+//2 Square
+void Z_Type(vector<vector<int>> table)
+{
+	int sum = 0;
+	for (int col = 1;col < table.size() - 1;col++)
+	{
+		for (int row = 0;row < table[0].size() - 1;row++)
+		{
+			sum = table[col][row] + table[col][row + 1];
+			vector<int> v(4, 0);
+			v[0] = table[col - 1][row];
+			v[1] = table[col - 1][row + 1];
+			v[2] = table[col + 1][row];
+			v[3] = table[col + 1][row + 1];
+			sort(v.begin(), v.end());
+			sum += v[3] + v[2];
+			ans = (sum > ans) ? sum : ans;
+		}
+	}
+}
+
 
 //Tanspose
 vector<vector<int>> Transpose(vector<vector<int>> table)
@@ -55,44 +88,19 @@ int main(int argc, char** argv)
 {
 	freopen("input.txt", "r", stdin);
 	int col, row;
-	int dx[4] = { -1,1,0,0 }, dy[4] = { 0,0,-1,1 };
 	cin >> col >> row;
 	vector<vector<int>> arr(col, vector<int>(row, 0));
 	for (int i = 0;i < col;i++)
 		for (int j = 0;j < row;j++)
 			cin >> arr[i][j];
 
-	for (int i = 0;i < arr.size();i++)
-	{
-		for (int j = 0;j < arr[0].size();j++)
-		{
-			vector<vector<bool>> memo(arr.size(),vector<bool> (arr[0].size(),0));	memo[i][j] = 1;
-			Node first = { i,j,1, arr[i][j] };
-			queue<Node> q;q.push(first);
-			while (!q.empty())
-			{
-				Node n = q.front();q.pop();
-				if (n.cnt >= 4)
-				{
-					ans = (n.sum > ans) ? n.sum : ans;
-					continue;
-				}
-				for (int k = 0;k < 4;k++)
-				{
-					int x = n.x + dx[i], y = n.y + dy[i];
-					if ((x >= 0) && (x < memo.size()) && (y >= 0) && (y < memo[0].size()))
-					{
-						memo[i][j] = true;
-						q.push(Node{x,y,n.cnt+1,n.sum+arr[x][y]});
-					}
-
-				}
-			}
-		}
-	}
-
 	T_Type(arr);
-	T_Type(Transpose(arr));
+	I_Type(arr);
+	Z_Type(arr);
+	vector < vector<int>> trans = Transpose(arr);
+	T_Type(trans);
+	I_Type(trans);
+	Z_Type(trans);
 	cout << ans;
 	return 0;
 }
