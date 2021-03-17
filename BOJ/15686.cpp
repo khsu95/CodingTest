7ec0e8f90c343fd8c9a0a950f2da081e050342e1
khsu95 <48276591+khsu95@users.noreply.github.com>
2021-03-17 22:05:28 +0900

    24ms

--- BOJ/15686.cpp
+++ BOJ/15686.cpp
@@ -4,9 +4,19 @@
 #include <iostream>
 #include <algorithm>
 #include <vector>
+#include <numeric>
 
 using namespace std;
 typedef pair<int, int> ii;
+
+int Min(vector<int> home, vector<bool> store)
+{
+	int min = 9999;
+	for (int i = 0;i < home.size();i++)
+		if (store[i])
+			min = (home[i] < min) ? home[i] : min;
+	return min;
+}
 
 int main(int argc, char** argv)
 {
@@ -27,7 +37,7 @@ int main(int argc, char** argv)
 			{
 				home_list.push_back(ii(i, j));
 				vector<int> temp;
-				for (int k = 0;j < store_list.size();k++)
+				for (int k = 0;k < store_list.size();k++)
 					temp.push_back(abs(store_list[k].first - i) + abs(store_list[k].second - j));
 				dist.push_back(temp);
 			}
@@ -35,15 +45,25 @@ int main(int argc, char** argv)
 			{
 				store_list.push_back(ii(i, j));
 				for (int k = 0;k < home_list.size();k++)
-					dist[k].push_back(abs(home_list[k].first - i) + abs(store_list[k].second - j));
+					dist[k].push_back(abs(home_list[k].first - i) + abs(home_list[k].second - j));
 			}
 		}
 	}
 
 	//Combination
-	vector<int> index(store_list.size());
 	vector<bool> comb(store_list.size(),0);
 	fill(comb.end() - m, comb.end(), true);
+	int ans = 9999;
+	do
+	{
+		int temp_ans = 0;
+		for (int i = 0;i < home_list.size();i++)
+		{
+			temp_ans += Min(dist[i], comb);
+		}
+		ans = (temp_ans < ans) ? temp_ans : ans;
+
+	} while (next_permutation(comb.begin(), comb.end()));
 	cout << ans;
 	return 0;
 }
