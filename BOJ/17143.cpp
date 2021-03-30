6b6d8ba86bff651299d13e37136ab69d83a8dbb7
khsu95 <48276591+khsu95@users.noreply.github.com>
2021-03-30 22:27:46 +0900

    Error

--- BOJ/17143.cpp
+++ BOJ/17143.cpp
@@ -1,5 +1,11 @@
 //https://www.acmicpc.net/problem/14888
 //2021.03.30
+/*
+////Counter Case
+4 2 2
+2 2 3 1 1
+4 2 3 1 2
+*/
 #define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
 #include <vector>
