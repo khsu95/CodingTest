336bca264f7e67e06bab45e5de8142fcc35ff2fd
khsu95 <48276591+khsu95@users.noreply.github.com>
2021-03-31 21:49:47 +0900

    fix segment fault and add condition

--- BOJ/17140.cpp
+++ BOJ/17140.cpp
@@ -20,8 +20,8 @@ vector<vector<int>> trans(vector<vector<int>> t)
 bool Compare(ii a, ii b)
 {
 	if (a.second != b.second)
-		return a.second > b.second;
-	return a.first > b.second;
+		return a.second < b.second;
+	return a.first < b.first;
 }
 
 int main(int argc, char** argv)
@@ -37,13 +37,16 @@ int main(int argc, char** argv)
 		for (int j = 0;j < 3;j++)
 			cin >> b[i][j];
 
-	while (b[t_row][t_col] != t_num)
+	while (1)
 	{
 		if (answer > 100)
 		{
 			answer = -1;
 			break;
 		}
+		if ((t_row < b.size()) && (t_col < b[0].size()))
+			if (b[t_row][t_col] == t_num)
+				break;
 
 		bool flag = 0;
 		if (b.size() < b[0].size())
@@ -58,33 +61,29 @@ int main(int argc, char** argv)
 			vector<ii> temp_row;
 			for (int j = 0;j < b[0].size();j++)
 			{
+				if (!b[i][j])	continue;
 				int idx = 0;
+				bool temp_row_flag = 0;
 				for (idx = 0;idx < temp_row.size();idx++)
 				{
 					if (temp_row[idx].first == b[i][j])
 					{
 						temp_row[idx].second++;
-						idx--;
+						temp_row_flag = 1;
 						break;
 					}
 				}
-				if (idx >= temp_row.size())
+				if (!temp_row_flag)
 					temp_row.push_back(ii(b[i][j], 1));
 			}
-	
+
 			sort(temp_row.begin(), temp_row.end(), Compare);
 			vector<ii>::iterator iter = temp_row.begin();
 			vector<int> n_row;
-			while (iter != temp_row.end())
+			while ((iter != temp_row.end())&&(n_row.size()<=100))
 			{
-				int cnt = 0;
-				while ((*iter).second > cnt)
-				{
-					n_row.push_back((*iter).first);
-					cnt++;
-				}
-				if (cnt > 1)
-					n_row.push_back((*iter).second);
+				n_row.push_back((*iter).first);
+				n_row.push_back((*iter).second);
 				iter++;
 			}
 			n_b.push_back(n_row);
@@ -96,8 +95,8 @@ int main(int argc, char** argv)
 			maxi = (maxi > b[i].size()) ? maxi : b[i].size();
 		for (int i = 0;i < b.size();i++)
 			for (int j = b[i].size();j < maxi;j++)
-				b[j].push_back(0);
-
+				b[i].push_back(0);
+		
 		if (flag)
 			b = trans(b);
 		answer++;
