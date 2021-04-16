f5836b161fc2f02923788963cd0c161e54a41aa1
khsu95 <48276591+khsu95@users.noreply.github.com>
2021-04-16 17:26:08 +0900

    call by reference

--- Programmers/Lv4/도둑질_DP.cpp
+++ Programmers/Lv4/도둑질_DP.cpp
@@ -4,26 +4,28 @@
 
 using namespace std;
 
-int DFS_DP(vector<int> &memo, vector<int> arr, int th)
+int DFS_DP(vector<int> &memo, vector<bool> &visit, vector<int> &arr, int th)
 {
     if(th>=memo.size()) return 0;
-    if(memo[th])    return memo[th];
-    return memo[th]=max(DFS_DP(memo,arr,th+2),DFS_DP(memo,arr,th+3))+arr[th];
+    if(visit[th])    return memo[th];
+    visit[th]=1;
+    return memo[th]=max(DFS_DP(memo, visit, arr, th+2),DFS_DP(memo, visit, arr, th+3))+arr[th];
 }
 
 int solution(vector<int> money) 
 {
     int answer = 0;
     vector<int> ans(money.size()-1,0);
-    DFS_DP(ans,money,0);
+    vector<bool> visit(money.size(),0);
+    DFS_DP(ans,visit, money, 0);
     answer=ans[0];
     
     fill(ans.begin(),ans.end(),0);ans.push_back(0);
-    DFS_DP(ans,money,1);
+    fill(visit.begin(),visit.end(),0);
+    DFS_DP(ans, visit, money, 1);
     answer=(ans[1]>answer)?ans[1]:answer;
     
-    fill(ans.begin(),ans.end(),0);
-    DFS_DP(ans,money,2);
+    DFS_DP(ans,visit, money,2);
     answer=(ans[2]>answer)?ans[2]:answer;
     return answer;
 }
