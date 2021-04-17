27665f0a46a36fc2022155cad407acffbaa99264
khsu95 <48276591+khsu95@users.noreply.github.com>
2021-04-18 04:27:49 +0900

    TLE score:76.7

--- Programmers/Lv3/합승택시요금.cpp
+++ Programmers/Lv3/합승택시요금.cpp
@@ -6,7 +6,7 @@
 using namespace std;
 typedef pair<int,int> ii;
 
-vector<int> Dij(int n, int s, vector<vector<int>> &map)
+vector<int> Dij(int n, int s, vector<vector<ii>> &map)
 {
     priority_queue<ii> pq;
     vector<int> v(n+1,9999999);
@@ -16,18 +16,11 @@ vector<int> Dij(int n, int s, vector<vector<int>> &map)
         ii elem=pq.top();pq.pop();
         if(elem.first>=v[elem.second])  continue;
         v[elem.second]=elem.first;
-        for(int i=0;i<map.size();i++)
+        for(int i=0;i<map[elem.second].size();i++)
         {
-            if(map[i][0]==elem.second)
-            {
-                if(elem.first+map[i][2]<v[map[i][1]])
-                    pq.push(ii(elem.first+map[i][2],map[i][1]));
-            }
-            else if(map[i][1]==elem.second)
-            {
-                if(elem.first+map[i][2]<v[map[i][0]])
-                    pq.push(ii(elem.first+map[i][2],map[i][0]));
-            }
+            int dist=elem.first+map[elem.second][i].second;
+            if(dist<v[map[elem.second][i].first])
+                pq.push(ii(dist,map[elem.second][i].first));
         }
     }
     return v;
@@ -36,9 +29,16 @@ vector<int> Dij(int n, int s, vector<vector<int>> &map)
 int solution(int n, int s, int a, int b, vector<vector<int>> fares) 
 {
     int answer = 9999999;
-    vector<int> d=Dij(n,s,fares);
-    vector<int> e=Dij(n,a,fares);
-    vector<int> f=Dij(n,b,fares);
+    vector<vector<ii>> table(n+1);
+    for(int i=0;i<fares.size();i++)
+    {
+        table[fares[i][0]].push_back(ii(fares[i][1],fares[i][2]));
+        table[fares[i][1]].push_back(ii(fares[i][0],fares[i][2]));
+    }
+    
+    vector<int> d=Dij(n,s,table);
+    vector<int> e=Dij(n,a,table);
+    vector<int> f=Dij(n,b,table);
     for(int i=1;i<=n;i++)
     {
         answer=(d[i]+e[i]+f[i]<answer)?d[i]+e[i]+f[i]:answer;
