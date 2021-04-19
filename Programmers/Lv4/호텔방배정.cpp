edcbb5f74ff8fda1afc0144c8351927c5a7d8fda
khsu95 <48276591+khsu95@users.noreply.github.com>
2021-04-19 20:39:09 +0900

    just little time saved

--- Programmers/Lv4/호텔방배정.cpp
+++ Programmers/Lv4/호텔방배정.cpp
@@ -1,5 +1,3 @@
-//https://programmers.co.kr/learn/courses/30/lessons/64063
-//2021.04.19
 #include <string>
 #include <vector>
 #include <map>
@@ -10,7 +8,7 @@ vector<long long> solution(long long k, vector<long long> room_number)
 {
 	vector<long long> answer;
 	map<long long, long long> s;
-	map<long long, long long>::iterator iter_left, iter_right;
+	map<long long, long long>::iterator iter_left, iter_right, iter_end;
 	for (int i = 0;i < room_number.size();i++)
 	{
 		long long n = room_number[i];
@@ -21,10 +19,11 @@ vector<long long> solution(long long k, vector<long long> room_number)
 			iter_right = s.find(n);
 		}
 		iter_right = s.insert(make_pair(n, n+1)).first;
-		if (s.find(n + 1) != s.end())
+        iter_end=s.end();
+		if (s.find(n + 1) != iter_end)
 			iter_right->second = s.find(n + 1)->second;
 		
-        if(iter_left!=s.end())
+        if(iter_left!=iter_end)
 			(*iter_left).second = (*iter_right).second;
         
 		answer.push_back(n);
